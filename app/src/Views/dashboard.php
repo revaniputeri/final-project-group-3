64aaef1c57f363311bf099e7c247e6ac44f93636
revaniputeri <revaninandaputeri@gmail.com>
2024-11-27 17:27:08 +0700

    refactor(dashboard): translate achievement labels to Indonesian

--- app/src/Views/dashboard.php
+++ app/src/Views/dashboard.php
@@ -20,13 +20,13 @@
                 <div class="col-md-4 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Add Achievement</h4>
+                            <h4 class="card-title">Tambah Prestasi</h4>
                             <p class="card-description">
-                                Click here to add new achievement
+                                Klik disini untuk menambah prestasi baru
                             </p>
                             <a href="/dashboard/achievement/form" class="btn btn-primary btn-lg btn-block">
                                 <i class="ti-plus mr-2"></i>
-                                Add New
+                                Tambah Baru
                             </a>
                         </div>
                     </div>
@@ -36,13 +36,13 @@
                 <div class="col-md-4 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Achievement History</h4>
+                            <h4 class="card-title">Riwayat Prestasi</h4>
                             <p class="card-description">
-                                View your achievement history
+                                Lihat riwayat prestasi Anda
                             </p>
                             <a href="/dashboard/achievement/history" class="btn btn-info btn-lg btn-block">
                                 <i class="ti-list mr-2"></i>
-                                View History
+                                Lihat Riwayat
                             </a>
                         </div>
                     </div>
@@ -52,13 +52,13 @@
                 <div class="col-md-4 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Achievement Information</h4>
+                            <h4 class="card-title">Informasi Prestasi</h4>
                             <p class="card-description">
-                                Check your achievement statistics
+                                Periksa statistik prestasi Anda
                             </p>
                             <a href="/dashboard/achievement/info" class="btn btn-success btn-lg btn-block">
                                 <i class="ti-bar-chart mr-2"></i>
-                                View Statistics
+                                Lihat Statistik
                             </a>
                         </div>
                     </div>
